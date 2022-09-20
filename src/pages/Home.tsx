02db10f038241c7e9fb4d32c7b91267ac539f21d
Miho <71237103+pluto0004@users.noreply.github.com>
2022-09-20 11:34:49 +0900

    Add GA tag (#5)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,29 @@
 import { Navbar } from "components/Navbar";
 import { Header } from "components/Header";
 import { Footer } from "components/Footer";
+import { Helmet } from "react-helmet";
+import { GA_ID } from "lib/gtag";
 
 const Home = () => {
   return (
     <>
+      {/* Google Analytics */}
+      {GA_ID && (
+        <Helmet>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { send_page_view: false });
+            `}
+          </script>
+        </Helmet>
+      )}
       <Header />
       <Navbar />
       <section className='hidden justify-center md:mt-28 md:flex'>
